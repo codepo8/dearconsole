0163be75f233cc3a845c5cfebb77b3ae07dd280d
Christian Heilmann <chris.heilmann@gmail.com>
2022-09-27 18:56:44 +0200

    back to index

--- assets/scripts.js
+++ assets/scripts.js
@@ -3,18 +3,21 @@ copypopup.classList.add('popup');
 copypopup.textContent = '☑️ Copied!';
 document.body.appendChild(copypopup);
 
-showcopied = _ => {
+copied = _ => {
+    e.preventDefault();
+    const snippet = e.target.parentNode.dataset.snippet;
+    navigator.clipboard.writeText(snippet);
+    console.log(snippet);
     document.body.classList.add('copied');
     setTimeout(() => document.body.classList.remove('copied'), 1500);
 };
 
 document.querySelector('#snippets')?.addEventListener('click', e => {
 if (e.target.tagName === 'svg') {
-    e.preventDefault();
-    const snippet = e.target.parentNode.dataset.snippet;
-    navigator.clipboard.writeText(snippet);
-    console.log(snippet);
-    showcopied();
+    copied();
+}
+if (e.target.tagName === 'BUTTON') {
+    copied();
 }
 });
 
